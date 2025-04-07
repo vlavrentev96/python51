098b51cec3d3b3c5154a309f55ef23880f181fd4
vlavrentev96 <vlavrentev96@mail.ru>
2025-04-07 19:09:39 +0300

    New
    Python

--- main.py
+++ main.py
@@ -8,4 +8,23 @@
 #Калькулятор
 value1 = int(input("Введите первую переменную:"))
 value2 = int(input("Введите вторую переменную:"))
-print(value1 + value2)#сумма элементов
+print(f"{value1} + {value2} = {value1 + value2}")#сумма элементов
+#string - тип данных для текста
+#int - тип данных для целочисленных чисел
+#float - тип данных с плавающей точкой
+#bool - тип данных логический, True, False
+print(f"{value1} - {value2} = {value1 - value2}")
+print(f"{value1} * {value2} = {value1 * value2}")
+print(f"{value1} / {value2} = {value1 / value2}")
+print(f"{value1} // {value2} = {value1 // value2}")
+print(f"{value1} % {value2} = {value1 % value2}")
+print(f"{value1} ** {value2} = {value1 ** value2}")
+print("*" * 11)
+#Проверка и сравнение данных
+print(value1 > value2)
+print(value1 < value2)
+print(value1 ==value2)
+print(value1 == 'Hello world!!!') #Проверка на равенства
+print("hello" > "hello world!!!") #Проверка на неравенства
+print("hello" > "hello world")
+
